44d94b6a2cf718fb2bd5deeedba0c21cb1e3b0f3
Greenbirch2007 <291109028@qq.com>
2019-08-06 18:35:45 +0800

    菜鸟教程过完

---
+++ T/js/j2.js
@@ -0,0 +1,83 @@
+// var vm = new Vue({
+// //     el: '#vue_det',
+// //     data: {
+// //         site: "菜鸟教程",
+// //         url: "www.runoob.com",
+// //         alexa: "10000"
+// //     },
+// //     methods: {
+// //         details: function() {
+// //             return  this.site + " - 学的不仅是技术，更是梦想！";
+// //         }
+// //     }
+// // })
+
+
+// new Vue({
+//     el:"#app",
+//     data:{
+//         seen:true,
+//         ok:true
+//     }
+// })
+
+// new Vue({
+// //     el:"#app"
+// // })
+
+
+// new Vue({
+//     el:"#app",
+//     data:{
+//         type:"C"
+//     }
+// })
+
+// new Vue({
+//     el:"#app",
+//     data:{
+//         ok:true
+//     }
+// })
+//
+// new Vue({
+//     el:"#app",
+//     data:{
+//         sites:[
+//             {name:"runoob"},
+//             {name:"Google"},
+//             {name:"Facebook"}
+//
+//         ]
+//     }
+// })
+
+// new Vue({
+// //     el:"#app",
+// //     data:{
+// //         object:{
+// //             name:"菜鸟教程",
+// //             url:"http://www.runoob.com",
+// //             slgan:"学的不仅是技术，俄式梦想"
+// //         }
+// //     }
+// // })
+
+// var vm = new Vue({
+//     el:"#app",
+//     data:{
+//         counter:1
+//     }
+// });
+//
+//
+// vm.watch('couter',function (nval,oval) {
+//     alert('计数器值的变化：'+oval+'变为'+nval+'!');
+// });
+
+new Vue({
+    el:"#app",
+    data:{
+        isActive:true
+    }
+})
